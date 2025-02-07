5648fc28dac8475c83b27640189bad5b16169a94
Ana Karla <145170013+Devaks99@users.noreply.github.com>
2025-02-06 21:56:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -181,3 +181,21 @@ function filterGenre(genreSelected) {
     s('.game-area').innerHTML = '';
     addGamesIntoDom();
 }
+
+
+// button scroll-top
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Adiciona a rolagem suave
+    });
+}
+
+window.addEventListener('scroll', function () {
+    var scrollTopButton = document.querySelector('.scroll-top');
+    if (this.window.pageYOffset > 200) {
+        scrollTopButton.style.display = 'block';
+    } else {
+        scrollTopButton.style.display = 'none';
+    }
+});
--- sobre.js
+++ sobre.js
@@ -11,12 +11,13 @@ cards.forEach(card => {
   });
 });
 
-// Clique na img e ela expande
+
+// Modal de Imagem
 const modal = document.getElementById('modal');
 const modalImg = document.getElementById('modal-img');
 const closeBtn = document.getElementById('close');
 
-cards.forEach(card => {
+document.querySelectorAll('.img-card').forEach(card => {
   card.addEventListener('click', () => {
     const imgSrc = card.querySelector('img').src;
     modalImg.src = imgSrc;
@@ -28,20 +29,24 @@ closeBtn.addEventListener('click', () => {
   modal.style.display = 'none';
 });
 
-// Scroll to Top
-const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+// Fechar modal ao clicar fora
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
 
+
+
+// Scroll-top Button
 window.addEventListener('scroll', () => {
-  if (window.pageYOffset > 200) {
-    scrollToTopBtn.classList.add('show');
-} else {
-    scrollToTopBtn.classList.remove('show');
-}
+  const scrollTop = document.querySelector('.scroll-top');
+  scrollTop.classList.toggle('active', window.scrollY > 200);
 });
 
-scrollToTopBtn.addEventListener('click', () => {
-window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-});
-});
+function scrollToTop() {
+  window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+  });
+}
